ab5ebd72515be9f5f8b80d5a280628733616c35f
Hinako00000 <118382458+Hinako00000@users.noreply.github.com>
2023-07-14 21:32:18 +0300

    u

--- body.py
+++ body.py
@@ -1,1 +1,7 @@
-print "Hello World"
+$ mkdir myproject
+$ cd myproject
+$ python3 -m venv venv
+
+$ . venv/bin/activate
+
+$ pip install Flask
--- home.py
+++ home.py
@@ -4,3 +4,16 @@ from email.iterators import body_line_iterator
 'title' :'QuizCat'
 <body>
 </body_line_iterator>
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
