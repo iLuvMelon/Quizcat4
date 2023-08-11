451d3776bac64567c045ff07c607fa290c3afb12
iLuvMelon <118382452+iLuvMelon@users.noreply.github.com>
2023-08-11 18:48:33 +0100

    WORLD PEACE IS A CULTT
    also I made a flashcard maker :D just have to find a way to link this to the hub

--- flashcards/js/app.js
+++ flashcards/js/app.js
@@ -2,6 +2,7 @@ const term = document.querySelector(".term")
 const definition = document.querySelector(".definition")
 const checkButton = document.querySelector(".check")
 const nextButton = document.querySelector(".next")
+const Button = document.querySelector(".button")
 var areyouthere = 0
 
 words = {
@@ -24,7 +25,16 @@ function getRandomWord() {
 
 
 checkButton.addEventListener("click", function() {
+    if (areyouthere < 2) {
+        areyouthere = 3
         definition.style.display = "block";
+        // Button.innerHTML = `<button class="check">Hide Definition</button> <button class="next">Next Definition</button>`
+    } else {
+        areyouthere = 0
+        definition.style.display = "none";
+        // Button.innerHTML = `<button class="check">Check Definition</button> <button class="next">Next Definition</button>`
+    }
+
 });
 
 nextButton.addEventListener("click", function() {
